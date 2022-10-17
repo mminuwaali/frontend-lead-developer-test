91d2b9c3d9516173768ce531b0b2033896936f5c
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2022-10-17 17:05:08 +0100

    finishing touches

--- src/stores/investigation.js
+++ src/stores/investigation.js
@@ -2,90 +2,10 @@ import { reactive } from 'vue';
 import { get, post } from '../util';
 import { defineStore } from 'pinia';
 
-const raw = [
-  {
-    "id": 1,
-    "title": "X-Ray",
-    "investigations": [
-      {
-        "id": 1,
-        "investigation_type_id": 1,
-        "title": "Chest"
-      },
-      {
-        "id": 2,
-        "investigation_type_id": 1,
-        "title": "Cervical Vertebrae"
-      },
-      {
-        "id": 3,
-        "investigation_type_id": 1,
-        "title": "Thoracic Vertebrae"
-      },
-      {
-        "id": 4,
-        "investigation_type_id": 1,
-        "title": "Lumbar Vartebrae"
-      },
-      {
-        "id": 5,
-        "investigation_type_id": 1,
-        "title": "Lumbo Sacral Vertebrae"
-      },
-      {
-        "id": 6,
-        "investigation_type_id": 1,
-        "title": "Wrist Joint"
-      },
-      {
-        "id": 7,
-        "investigation_type_id": 1,
-        "title": "Ankle"
-      },
-      {
-        "id": 8,
-        "investigation_type_id": 1,
-        "title": "Fingers"
-      },
-      {
-        "id": 9,
-        "investigation_type_id": 1,
-        "title": "Toes"
-      }
-    ]
-  },
-  {
-    "id": 2,
-    "title": "Ultrasound Scan",
-    "investigations": [
-      {
-        "id": 10,
-        "investigation_type_id": 2,
-        "title": "Breast"
-      },
-      {
-        "id": 11,
-        "investigation_type_id": 2,
-        "title": "Pelvis"
-      },
-      {
-        "id": 12,
-        "investigation_type_id": 2,
-        "title": "Prostate"
-      },
-      {
-        "id": 13,
-        "investigation_type_id": 2,
-        "title": "Thyroid"
-      }
-    ]
-  }
-]
-
 export default defineStore('investigation', () =>
 {
   // state
-  const investigation = reactive({ load: 0, error: 0, data: [...raw] });
+  const investigation = reactive({ load: 0, error: 0, data: [] });
 
   // methods
   const $get = async () =>
